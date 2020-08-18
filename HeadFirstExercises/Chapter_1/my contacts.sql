bda3de16b612f61695790601c53c05c82bcadffd
Anjali <anjali.payal69@gmail.com>
2020-08-18 10:06:44 +0200

    Added mySQL files

---
+++ HeadFirstExercises/Chapter_1/my contacts.sql
@@ -0,0 +1,8 @@
+INSERT INTO my_contacts
+VALUES
+('Funyon','Steve','steve@onionflavoredrings.com', 'M', '1970-04-01', 'Punk','Grover\'s Mill, NJ','Single','smashing the state','compatriots,guitar players');
+SELECT * FROM my_contacts
+WHERE location = 'Grover''s Mill, NJ';
+
+
+
